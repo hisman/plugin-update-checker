466e4b647168e3b4a209118b3baa144d1234ef51
Hisman <hisman.ns@gmail.com>
2025-08-25 20:21:08 +0700

    Move the flag to plugin update class

--- Puc/v5p6/Update.php
+++ Puc/v5p6/Update.php
@@ -16,13 +16,12 @@ if ( !class_exists(Update::class, false) ):
 		public $version;
 		public $download_url;
 		public $translations = array();
-		public $auto_update_forced;
 
 		/**
 		 * @return string[]
 		 */
 		protected function getFieldNames() {
-			return array('slug', 'version', 'download_url', 'translations', 'auto_update_forced');
+			return array('slug', 'version', 'download_url', 'translations');
 		}
 
 		public function toWpFormat() {
@@ -31,7 +30,6 @@ if ( !class_exists(Update::class, false) ):
 			$update->slug = $this->slug;
 			$update->new_version = $this->version;
 			$update->package = $this->download_url;
-			$update->{'auto-update-forced'} = $this->auto_update_forced;
 
 			return $update;
 		}
